951705b20453559490392869f360d9dc843473e0
ng <ngnaven@gmail.com>
2021-08-10 17:15:58 +0530

    001:WIP

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -15,7 +17,9 @@ func main() {
 	r.Use(cors.Default())
 
 	// use r.Group for grouping routes
-	r.GET("/", getHandler)
+	r.GET("/", HelloHandler)
+	r.GET("/user", GetName)
+	r.GET("/profile/:id", GetId)
 	// r.GET("/user", func(c *gin.Context) {
 	// 	firstname := c.Query("firstname")
 	// 	lastname := c.Query("lastname")
@@ -29,13 +33,33 @@ func main() {
 	}
 }
 
-func getHandler(c *gin.Context) {
-	// H is map[string]interface{}
-	// so value can be anything
-	// c.JSON writes it to the response handler
+// H is map[string]interface{}
+// so value can be anything
+// c.JSON writes it to the response handler
 
+func HelloHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message":  true,
 		"message2": "hello world",
 	})
+}
+
+// localhost:9000/user?firstname=naveen&lastname=garg
+func GetName(c *gin.Context) {
+	firstName := c.Query("firstname")
+	lastName := c.Query("lastname")
+
+	c.JSON(http.StatusOK, gin.H{
+		"firstname": firstName,
+		"lastname":  lastName,
+	})
+}
+
+func GetId(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	c.JSON(http.StatusOK, gin.H{
+		"id": id,
+	})
+	// c.JSON(200, id)
 }
